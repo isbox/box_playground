a97bbb0f36061838ffcf22ca6d5a656eae5d5135
box <thereisbox@foxmail.com>
2019-01-21 23:51:46 +0800

    homepage

---
+++ app/components/sidebar/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import { Layout } from "antd";
+const { Sider } = Layout;
+
+export default class SideBar extends PureComponent {
+	render() {
+		return <Sider>
+
+		</Sider>;
+	}
+}
--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, message } from 'antd';
 import { Link } from 'react-router';
 import { bindAll } from 'lodash';
+import Page from '@/components/page';
 import fetch from '@/lib/fetch';
 
 export default class extends Component {
@@ -34,11 +35,11 @@ export default class extends Component {
     }
 
     render() {
-        return <div>
+        return <Page>
             <Link to="/">首页</Link> <Link to="/test">test页</Link> <Link to="/test/demo01">demo01页</Link>
             <h2>i am index</h2>
             <p>now you are in index page!</p>
             <Button type="primary" onClick={this.info}>Display normal message</Button>
-        </div>;
+        </Page>;
     }
 }
