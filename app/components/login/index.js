772feed3625267617a30df85eddde4add9dcea31
box <thereisbox@foxmail.com>
2019-04-12 23:34:42 +0800

    axios改造

--- app/components/login/index.js
+++ app/components/login/index.js
@@ -9,49 +9,50 @@ const getUserStore = rootStore => ({userStore: rootStore.store.userStore});
 @inject(getUserStore)
 @observer
 class LoginModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: '登陆'
-        };
-        bindAll(this, [
-            'onCancel',
-            'onOk'
-        ]);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: '登陆'
+		};
+		bindAll(this, [
+			'onCancel',
+			'onOk'
+		]);
+	}
 
-    componentDidMount() {
-        this.props.userStore.openLogin();
-    }
+	componentDidMount() {
+		this.props.userStore.openLogin();
+	}
 
-    onCancel() {
-        this.props.userStore.closeLogin();
-    }
+	onCancel() {
+		this.props.userStore.closeLogin();
+	}
 
-    onOk() {
-        this.onCancel();
-    }
+	onOk() {
+    
+		this.onCancel();
+	}
 
-    render() {
-        // onCancel={cancelCallback}
-        const { loginModal, userInfo } = this.props.userStore;
-        const { modalWidth, userIcon, passwordIcon } = this.props;
-        const { title } = this.state;
+	render() {
+		// onCancel={cancelCallback}
+		const { loginModal, userInfo } = this.props.userStore;
+		const { modalWidth, userIcon, passwordIcon } = this.props;
+		const { title } = this.state;
 
-        return <Modal width={modalWidth} visible={loginModal} title={title} 
-                      onCancel={this.onCancel} onOk={this.onOk}>
-            <div className="login-modal">
-                <Input className="mb-10" placeholder="输入用账号/邮箱" prefix={userIcon}></Input>
-                <Input placeholder="输入密码" prefix={passwordIcon}></Input>
-            </div>
-        </Modal>;
-    }
+		return <Modal width={modalWidth} visible={loginModal} title={title}
+		              onCancel={this.onCancel} onOk={this.onOk}>
+			<div className="login-modal">
+				<Input className="mb-10" placeholder="输入用账号/邮箱" prefix={userIcon} />
+				<Input placeholder="输入密码" prefix={passwordIcon} />
+			</div>
+		</Modal>;
+	}
 }
 
 LoginModal.defaultProps = {
-    modalWidth: '20rem',
-    userIcon: <Icon type="user" className="tc-light-gray" />,
-    passwordIcon: <Icon type="key" className="tc-light-gray" />
+	modalWidth: '20rem',
+	userIcon: <Icon type="user" className="tc-light-gray" />,
+	passwordIcon: <Icon type="key" className="tc-light-gray" />
 };
 
 export default LoginModal;
--- app/lib/fetch.js
+++ app/lib/fetch.js
@@ -1,30 +1,33 @@
 import axios from 'axios';
 
+const instance = axios.create();
+instance.interceptors.request.use(() => {
+
+});
+
+instance.interceptors.response.use((res) => {
+	return res.data;
+});
+
 class Fetch {
 
-    static get(url, options = {}) {
-        options.url = url;
-        Fetch.fetch(options, 'get');
-    }
+	static get(url, options = {}) {
+		options.url = url;
+		Fetch.fetch(options, 'get');
+	}
 
-    static post(url, options = {}) {
-        options.url = url;
-        Fetch.fetch(options, 'post');
-    }
+	static post(url, options = {}) {
+		options.url = url;
+		Fetch.fetch(options, 'post');
+	}
 
-    static fetch(options, method) {
-        let mergeOpt = Object.assign({
-            method: options.method || method
-        }, options);
-
-        axios(options)
-            .then((resolve, reject) => {
-                console.log(resolve);
-            })
-            .catch(err => {
-
-            });
-    }
+	static fetch(options, method) {
+		options.method = method;
+		if (method.toLocaleLowerCase() === 'get') {
+			options.params = data;
+		}
+		return instance(options);
+	}
 }
 
 export default Fetch;
